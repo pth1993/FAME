3c83006f3387122bcb5f00d31de797fa0058cab5
pth1993 <phamthaihoang.hn@gmail.com>
2021-10-06 08:12:06 -0400

    first commit

--- FAME/main.py
+++ FAME/main.py
@@ -22,7 +22,7 @@ torch.backends.cudnn.deterministic = True
 atom_file = 'data/atom_dict.pkl'
 bond_file = 'data/bond_dict.pkl'
 fragment_file = 'data/fragment_dict.pkl'
-n_epoch = 2
+n_epoch = 1
 model_name = 'FAME'
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 multi_gpu = True
@@ -72,72 +72,76 @@ for e in range(n_epoch):
     train_loss = 0
     train_nll_loss = 0
     train_kld_loss = 0
+    print('Train')
     for i, b in enumerate(tqdm(data_loader_train)):
-        graph_drug = b['graph_drug'].to(device)
-        graph_frag = b['graph_frag'].to(device)
-        batch_frag = b['batch_frag']
-        label = b['label'].to(device)
-        output, mu, logvar = model.forward(graph_drug, graph_frag, batch_frag, label)
-        if isinstance(model, nn.DataParallel):
-            nll, kld = model.module.loss(output, label, batch_frag, mu, logvar)
-        else:
-            nll, kld = model.loss(output, label, batch_frag, mu, logvar)
-        w = 0.1
-        loss = nll + w * kld
-        loss.backward()
-        optimizer.step()
-        train_loss += loss.item()
-        train_nll_loss += nll.item()
-        train_kld_loss += kld.item()
-    print('Train loss: %.4f - NLL loss: %.4f - KLD loss: %.4f' % (train_loss / (i+1), train_nll_loss / (i+1),
-                                                                  train_kld_loss / (i+1)))
-    train_loss_list.append(train_loss / (i+1))
-    train_nll_loss_list.append(train_nll_loss / (i+1))
-    train_kld_loss_list.append(train_kld_loss / (i+1))
+        pass
+    #     graph_drug = b['graph_drug'].to(device)
+    #     graph_frag = b['graph_frag'].to(device)
+    #     batch_frag = b['batch_frag']
+    #     label = b['label'].to(device)
+    #     output, mu, logvar = model.forward(graph_drug, graph_frag, batch_frag, label)
+    #     if isinstance(model, nn.DataParallel):
+    #         nll, kld = model.module.loss(output, label, batch_frag, mu, logvar)
+    #     else:
+    #         nll, kld = model.loss(output, label, batch_frag, mu, logvar)
+    #     w = 0.1
+    #     loss = nll + w * kld
+    #     loss.backward()
+    #     optimizer.step()
+    #     train_loss += loss.item()
+    #     train_nll_loss += nll.item()
+    #     train_kld_loss += kld.item()
+    # print('Train loss: %.4f - NLL loss: %.4f - KLD loss: %.4f' % (train_loss / (i+1), train_nll_loss / (i+1),
+    #                                                               train_kld_loss / (i+1)))
+    # train_loss_list.append(train_loss / (i+1))
+    # train_nll_loss_list.append(train_nll_loss / (i+1))
+    # train_kld_loss_list.append(train_kld_loss / (i+1))
 
     model.eval()
     with torch.no_grad():
         val_loss = 0
         val_nll_loss = 0
         val_kld_loss = 0
+        print('Val')
         for i, b in enumerate(tqdm(data_loader_val)):
-            graph_drug = b['graph_drug'].to(device)
-            graph_frag = b['graph_frag'].to(device)
-            batch_frag = b['batch_frag']
-            label = b['label'].to(device)
-            output, mu, logvar = model.forward(graph_drug, graph_frag, batch_frag, label)
-            if isinstance(model, nn.DataParallel):
-                nll, kld = model.module.loss(output, label, batch_frag, mu, logvar)
-            else:
-                nll, kld = model.loss(output, label, batch_frag, mu, logvar)
-            w = 0.1
-            loss = nll + w * kld
-            val_loss += loss.item()
-            val_nll_loss += nll.item()
-            val_kld_loss += kld.item()
-    print('Val loss: %.4f - NLL loss: %.4f - KLD loss: %.4f' % (val_loss / (i+1), val_nll_loss / (i+1),
-                                                                val_kld_loss / (i+1)))
-    val_loss_list.append(val_loss / (i+1))
-    val_nll_loss_list.append(val_nll_loss / (i+1))
-    val_kld_loss_list.append(val_kld_loss / (i+1))
-    if (val_loss / (i+1)) < best_val_loss:
-        best_val_loss = val_loss / (i+1)
-        best_epoch = e
-        torch.save({'encoder_state_dict': model.module.graph_encoder.state_dict() if isinstance(model, nn.DataParallel)
-        else model.graph_encoder.state_dict(), 'decoder_state_dict': model.module.graph_decoder.state_dict()
-        if isinstance(model, nn.DataParallel) else model.graph_decoder.state_dict(),
-                    'optimizer_state_dict': optimizer.state_dict()},
-                   'saved_model/pt/%s_%d.ckpt' % (model_name, best_epoch))
-
-print("Loss / NLL / KLD on val set by epoch %d (best val epoch): %.4f - %.4f - %.4f"
-      % (best_epoch + 1, val_loss_list[best_epoch], val_nll_loss_list[best_epoch],
-         val_kld_loss_list[best_epoch]))
-
-df = pd.DataFrame(list(zip(train_loss_list, train_nll_loss_list, train_kld_loss_list, val_loss_list,
-                           val_nll_loss_list, val_kld_loss_list)),
-                  columns=['train_loss', 'train_nll_loss', 'train_kld_loss', 'val_loss', 'val_nll_loss', 'val_kld_loss'],
-                  index=np.arange(len(train_loss_list))+1)
-df.to_csv('output/pt/%s' % log_file)
+            pass
+#             graph_drug = b['graph_drug'].to(device)
+#             graph_frag = b['graph_frag'].to(device)
+#             batch_frag = b['batch_frag']
+#             label = b['label'].to(device)
+#             output, mu, logvar = model.forward(graph_drug, graph_frag, batch_frag, label)
+#             if isinstance(model, nn.DataParallel):
+#                 nll, kld = model.module.loss(output, label, batch_frag, mu, logvar)
+#             else:
+#                 nll, kld = model.loss(output, label, batch_frag, mu, logvar)
+#             w = 0.1
+#             loss = nll + w * kld
+#             val_loss += loss.item()
+#             val_nll_loss += nll.item()
+#             val_kld_loss += kld.item()
+#     print('Val loss: %.4f - NLL loss: %.4f - KLD loss: %.4f' % (val_loss / (i+1), val_nll_loss / (i+1),
+#                                                                 val_kld_loss / (i+1)))
+#     val_loss_list.append(val_loss / (i+1))
+#     val_nll_loss_list.append(val_nll_loss / (i+1))
+#     val_kld_loss_list.append(val_kld_loss / (i+1))
+#     if (val_loss / (i+1)) < best_val_loss:
+#         best_val_loss = val_loss / (i+1)
+#         best_epoch = e
+#         torch.save({'encoder_state_dict': model.module.graph_encoder.state_dict() if isinstance(model, nn.DataParallel)
+#         else model.graph_encoder.state_dict(), 'decoder_state_dict': model.module.graph_decoder.state_dict()
+#         if isinstance(model, nn.DataParallel) else model.graph_decoder.state_dict(),
+#                     'optimizer_state_dict': optimizer.state_dict()},
+#                    'saved_model/pt/%s_%d.ckpt' % (model_name, best_epoch))
+#
+# print("Loss / NLL / KLD on val set by epoch %d (best val epoch): %.4f - %.4f - %.4f"
+#       % (best_epoch + 1, val_loss_list[best_epoch], val_nll_loss_list[best_epoch],
+#          val_kld_loss_list[best_epoch]))
+#
+# df = pd.DataFrame(list(zip(train_loss_list, train_nll_loss_list, train_kld_loss_list, val_loss_list,
+#                            val_nll_loss_list, val_kld_loss_list)),
+#                   columns=['train_loss', 'train_nll_loss', 'train_kld_loss', 'val_loss', 'val_nll_loss', 'val_kld_loss'],
+#                   index=np.arange(len(train_loss_list))+1)
+# df.to_csv('output/pt/%s' % log_file)
 
 end_time = datetime.now()
 print(end_time - start_time)
--- FAME/utils/datareader.py
+++ FAME/utils/datareader.py
@@ -23,8 +23,11 @@ class MoleculeDataset(Dataset):
         return len(self.smiles_drug)
 
     def __getitem__(self, idx):
-        self.graph_drug = convert_mol_2_graph_pyg(self.mol_drug[idx], self.atom_dict, self.bond_dict)
-        self.graph_frag = [convert_mol_2_graph_pyg(frag, self.atom_dict, self.bond_dict) for frag in self.mol_frag[idx]]
+        try:
+            self.graph_drug = convert_mol_2_graph_pyg(self.mol_drug[idx], self.atom_dict, self.bond_dict)
+            self.graph_frag = [convert_mol_2_graph_pyg(frag, self.atom_dict, self.bond_dict) for frag in self.mol_frag[idx]]
+        except:
+            print(idx)
         sample = {'smiles_drug': self.smiles_drug[idx], 'smiles_frag': self.smiles_frag[idx],
                   'mol_drug': self.mol_drug[idx], 'mol_frag': self.mol_frag[idx], 'graph_drug': self.graph_drug,
                   'graph_frag': self.graph_frag, 'label': self.label[idx]}

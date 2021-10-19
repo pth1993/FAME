fe1d6ce65fcb49e4979b4d6c43bb4a6e5b125676
pth1993 <phamthaihoang.hn@gmail.com>
2021-10-19 12:34:18 -0400

    refactor

---
+++ FAME/data_download.sh
@@ -0,0 +1,13 @@
+dir="data"
+file="data.zip"
+if [ -d "$dir" ]; then
+	echo "$dir found."
+else
+	url="https://drive.google.com/uc?id=1z0-MpDrmYh5Gi17x4ZRbavsEaXIyu4oZ&export=download"
+    wget --load-cookie cookie.txt --save-cookie cookie.txt "${url}" -O tmp
+    c=`grep -o "confirm=...." tmp`
+    wget --load-cookie cookie.txt --save-cookie cookie.txt "${url}&$c" -O "${file}"
+    rm cookie.txt tmp
+    unzip "${file}" -d "${dir}"
+    rm "${file}"
+fi
